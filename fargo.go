7acc1f92a2ceb277cee0700461f40d08cb3fbd3c
Thirsty Fermat <frescura.peter@gmail.com>
2018-08-03 13:09:49 +0200

    add updates

--- fargo.go
+++ fargo.go
@@ -7,26 +7,28 @@ import (
 )
 
 func main() {
-	c := fargo.NewConn("http://192.168.99.100:8080/eureka/v2")
+	c := fargo.NewConn("http://localhost:8081/eureka/v2")
 
-	i := fargo.Instance{
-		HostName:         "i-6543",
-		Port:             9090,
-		App:              "TESTAPP",
-		IPAddr:           "127.0.0.10",
-		VipAddress:       "127.0.0.10",
-		SecureVipAddress: "127.0.0.10",
-		DataCenterInfo:   fargo.DataCenterInfo{Name: fargo.MyOwn},
-		Status:           fargo.UP,
-	}
-
-	c.RegisterInstance(&i)
+	// i := fargo.Instance{
+	// 	HostName:         "i-6543",
+	// 	Port:             9091,
+	// 	App:              "TESTAPP",
+	// 	IPAddr:           "127.0.0.1",
+	// 	VipAddress:       "127.0.0.1",
+	// 	SecureVipAddress: "127.0.0.1",
+	// 	DataCenterInfo:   fargo.DataCenterInfo{Name: fargo.MyOwn},
+	// 	Status:           fargo.UP,
+	// }
+	//
+	// c.RegisterInstance(&i)
 	f, _ := c.GetApps()
 
 	for key, theApp := range f {
 		fmt.Println("App:", key, " First Host Name:", theApp.Instances[0].HostName)
 	}
 
-	app, _ := c.GetApp("TESTAPP")
+	app, _ := c.GetApp("backing-catalog")
 	fmt.Printf("%v\n", app)
+	app, _ = c.GetApp("backing-fulfillment")
+	fmt.Printf("%v\n", app.HostName)
 }
